a69711ed27ac5acf3475504be0e5864d19315dad
Tom Ellis <tom-git@jaguarpaw.co.uk>
2019-07-20 13:52:41 +0100

    Identities

--- Op/Op.hs
+++ Op/Op.hs
@@ -4,43 +4,38 @@
 {-# LANGUAGE ExistentialQuantification #-}
 {-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE UndecidableInstances #-} --  Try to get rid of this
 
 import Control.Category
 import Prelude hiding (id, (.), (>>))
 
-class T arr s p tv | arr -> s p tv where
-  tsPush :: tv (s a b) `arr` s (tv a) (tv b)
-  tsPull :: s (tv a) (tv b) `arr` tv (s a b)
+class Category arr => T arr s p tv | arr -> s p tv where
+  sPush :: tv (s a b) `arr` s (tv a) (tv b)
+  pPush :: tv (p a b) `arr` p (tv a) (tv b)
+  flipT :: (a `arr` b) -> (b `arr` a)
 
-  tpPush :: tv (p a b) `arr` p (tv a) (tv b)
-  tpPull :: p (tv a) (tv b) `arr` tv (p a b)
+class Category arr => C arr varr v m t | arr -> varr v m t where
+  arrV  :: (a `varr` b) -> (v a `arr` v b)
+  arrTa :: (a `arr` b) -> (t a `arr` t b)
+  flipC :: (a `arr` b) -> (b `arr` a)
 
-class Category arr
-  => O arr tarr m _1 v s p t tv | arr -> tarr m v s p _1 t tv
-  where
-  assocR :: m (m a b) c
-            `arr` m a (m b c)
+  assoc :: m (m a b) c `arr` m a (m b c)
 
-  assocL :: m a (m b c)
-            `arr` m (m a b) c
-
-  tangentMu :: t (a `m` b) `arr` (t a `m` t b)
-  tangentMw :: (t a `m` t b) `arr` t (a `m` b)
-
+  tPush :: t (a `m` b) `arr` (t a `m` t b)
   tJoin :: t (t a) `arr` t a
-  tDup  :: t a `arr` t (t a)
 
   tVar   :: t (v a) `arr` v (tv a)
-  tUnvar :: v (tv a) `arr` t (v a)
 
-  tUnitE :: t _1 `arr` _1
-  tUnitI :: _1 `arr` t _1
+  tUnit :: t _1 `arr` _1
 
-  unitI :: a `arr` (_1 `m` a)
-
-  unitE :: (_1 `m` a) `arr` a
+  unit :: a `arr` (_1 `m` a)
 
   comm :: m a b `arr` m b a
+
+class Category arr
+  => O arr tarr m _1 v s p t | arr -> tarr m v s p _1 t
+  where
+  arrT :: (a `tarr` b) -> (a `arr` b)
 
   inl :: v a `arr` v (s a b)
 
@@ -53,11 +48,8 @@ class Category arr
 
   ignore :: a `arr` _1
 
-  pair :: v a `m` v b
-          `arr` v (a `p` b)
-
-  unpair :: v (a `p` b)
-            `arr` v a `m` v b
+  pair :: v a `m` v b `arr` v (a `p` b)
+  unpair :: v (a `p` b) `arr` v a `m` v b
 
   match :: ((ci `m` v ai1) `arr` (co `m` v ao1))
         -> ((ci `m` v ai2) `arr` (co `m` v ao2))
@@ -68,77 +60,28 @@ class Category arr
 
   add :: (t a `m` t a) `arr` t a
 
-foo :: O arr tarr m _1 v s p t tv
+foo :: (C tarr varr v m t, O arr tarr m _1 v s p t)
     => arr a1 z
     -> arr z a2
     -> arr (b1 `m` c1) (b2 `m` c2)
     -> arr ((a1 `m` b1) `m` c1) ((a2 `m` b2) `m` c2)
-foo l m r = assocR >>> ((l >>> m) |><| r) >>> assocL
+foo l m r = arrT assoc >>> ((l >>> m) |><| r) >>> arrT (flipC assoc)
 
-data R arr tarr m _1 (v :: * -> *) (s :: * -> * -> *) (p :: * -> * -> *) t
+data R arr (tarr :: * -> * -> *) m _1
+       (v :: * -> *) (s :: * -> * -> *) (p :: * -> * -> *) t
        (tv :: * -> *) a b =
   forall r. R (a `arr` (r `m` b)) ((r `m` t b) `arr` t a)
 
-instance O arr tarr m _1 v s p t tv
+instance (C tarr varr v m t, O arr tarr m _1 v s p t)
   => Category (R arr tarr m _1 v s p t tv) where
-  id = R (id >>> unitI) (unitE >>> id)
+  id = R (id >>> arrT unit) (arrT (flipC unit) >>> id)
 
   f . g = case f of
     R f1 f2 -> case g of
-      R g1 g2 -> R (assocL <<< (id |><| f1) <<< g1)
-                   (assocR >>> (id |><| f2) >>> g2)
+      R g1 g2 -> R (arrT (flipC assoc) <<< (id |><| f1) <<< g1)
+                   (arrT assoc >>> (id |><| f2) >>> g2)
 
-instance O arr tarr m _1 v s p t tv => O (R arr tarr m _1 v s p t tv) tarr m _1 v s p t tv where
-  assocR = R (assocR >>> unitI)
-             (unitE
-              >>> tangentMu
-              >>> (id |><| tangentMu)
-              >>> assocL
-              >>> (tangentMw |><| id)
-              >>> tangentMw)
-
-  assocL = R (assocL >>> unitI)
-             (unitE
-              >>> tangentMu
-              >>> (tangentMu |><| id)
-              >>> assocR
-              >>> (id |><| tangentMw)
-              >>> tangentMw)
-
-  tangentMu = R (tangentMu >>> unitI)
-                (unitE
-                 >>> tangentMu
-                 >>> (tJoin |><| tJoin)
-                 >>> tangentMw
-                 >>> tDup)
-
-  tangentMw = R (tangentMw >>> unitI)
-                (unitE
-                 >>> tJoin
-                 >>> tangentMu
-                 >>> (tDup |><| tDup)
-                 >>> tangentMw)
-
-  tJoin = R (tJoin >>> unitI)
-            (unitE >>> tDup)
-
-  tDup = R (tDup >>> unitI)
-           (unitE >>> tJoin)
-
-  unitI = R (unitI >>> unitI)
-            (unitE >>> tangentMu >>> (tUnitE |><| id) >>> unitE)
-
-  unitE = R (unitE >>> unitI)
-            (unitE >>> (tangentMw <<< (tUnitI |><| id) <<< unitI))
-
-  tUnitE = R (tUnitE >>> unitI)
-             (unitE >>> tDup) -- Not wholly convinced by this
-
-  tUnitI = R (tUnitI >>> unitI)
-             (unitE >>> tJoin) -- Not wholly convinced by this
-
-  comm = R (comm >>> unitI)
-           (unitE >>> tangentMu >>> comm >>> tangentMw)
-
-  inl = R (inl >>> unitI)
+instance (O arr tarr m _1 v s p t, C tarr varr v m t)
+  => O (R arr tarr m _1 v s p t tv) tarr m _1 v s p t where
+  inl = R (inl >>> arrT unit)
           undefined
