30921cfb8f91e1385a878e1b876fb4abc26b5034
Tom Ellis <tom-git@jaguarpaw.co.uk>
2019-07-21 12:23:01 +0100

    Switch to vs for trace

--- Op/Op.hs
+++ Op/Op.hs
@@ -52,15 +52,17 @@ class Category arr
 
   zero :: _1 `arr` t a
 
+  -- This probably belongs in a superclass
+  zeroV :: v u `arr` v (tv a)
+
   unitT :: _1 `arr` v u
 
   pair :: (v a `m` v b) `arr` v (a `p` b)
   unpair :: v (a `p` b) `arr` (v a `m` v b)
 
-  match :: ((ci `m` v ai1) `arr` (co `m` v ao1))
-        -> ((ci `m` v ai2) `arr` (co `m` v ao2))
-        -> ((ci `m` v (s ai1 ai2))
-            `arr` (co `m` v (s ao1 ao2)))
+  caseS :: ((ci `m` v ai1) `arr` co)
+        -> ((ci `m` v ai2) `arr` co)
+        -> ((ci `m` v (s ai1 ai2)) `arr` co)
 
   dup :: a `arr` (a `m` a)
 
@@ -76,7 +78,7 @@ foo l m r = arrT assoc >>> ((l >>> m) |><| r) >>> arrT (flipC assoc)
 data R arr (tarr :: * -> * -> *) m _1
        (v :: * -> *) (s :: * -> * -> *) (p :: * -> * -> *) t
        (u :: *) a b =
-  forall r. R (a `arr` (r `m` b)) ((r `m` t b) `arr` t a)
+  forall r. R (a `arr` (v r `m` b)) ((v r `m` t b) `arr` t a)
 
 instance (Monoidal arr m, C tarr varr v m t, O arr tarr m _1 v s p t u)
   => Category (R arr tarr m _1 v s p t u) where
@@ -84,30 +86,35 @@ instance (Monoidal arr m, C tarr varr v m t, O arr tarr m _1 v s p t u)
 
   f . g = case f of
     R f1 f2 -> case g of
-      R g1 g2 -> R (arrT (flipC assoc) <<< (id |><| f1) <<< g1)
-                   (arrT assoc >>> (id |><| f2) >>> g2)
+      R g1 g2 ->
+        R ((pair |><| id) <<< arrT (flipC assoc) <<< (id |><| f1) <<< g1)
+          ((unpair |><| id) >>> arrT assoc >>> (id |><| f2) >>> g2)
 
 instance (Monoidal arr m, Monoidal tarr m, O arr tarr m _1 v s p t u,
           C tarr varr v m t, T varr s p tv)
   => O (R arr tarr m _1 v s p t u) tarr m _1 v s p t u where
   arrT f = R (arrT (f >>> unit)) (arrT (flipC (arrTa f >>> unit)))
 
-  inl = R (inl >>> arrT unit)
+  inl = R (inl >>> arrT unit >>> (unitT |><| id))
           (bar >>> baz >>> quux)
 
   ignore = R (ignore >>> arrT unit)
              (arrT (flipC unit >>> tUnit) >>> zero)
 
-  pair = R (pair >>> arrT unit)
+  pair = R (pair >>> arrT unit >>> (unitT |><| id))
            flub
 
-  match = \f g -> case f of
+{-
+  caseS = \f g -> case f of
     R f1 f2 -> case g of
-      R g1 g2 -> R undefined undefined
+      R g1 g2 -> R (caseS (f1 >>> arrT comm >>> (id |><| inl))
+                          (g1 >>> arrT comm >>> (id |><| inr)))
+                   undefined
+-}
 
 flub :: (O arr tarr m _1 v s p t u, C tarr varr v m t, T varr s p tv,
          Monoidal tarr m)
-     => m _1 (t (v (p a b))) `arr` t (m (v a) (v b))
+     => m (v u) (t (v (p a b))) `arr` t (m (v a) (v b))
 flub = arrT (flipC unit)
        >>> arrT tVar
        >>> arrT (arrV pPush)
@@ -116,18 +123,18 @@ flub = arrT (flipC unit)
        >>> arrT (flipC tPush)
 
 bar :: (O arr tarr m _1 v s p t u, C tarr varr v m t, T varr s p tv)
-    => ((_1 `m` (t (v (s a b)))) `arr` (_1 `m` (v (s (tv a) (tv b)))))
+    => ((v u `m` (t (v (s a b)))) `arr` (_1 `m` (v (s (tv a) (tv b)))))
 bar = arrT (flipC unit >>> tVar >>> arrV sPush >>> unit)
 
 baz :: (Monoidal arr m, O arr tarr m _1 v s p t u, C tarr varr v m t,
         T varr s p tv)
-    => (_1 `m` (v (s (tv a) (tv b)))) `arr` (v (tv a) `m` v (s u u))
-baz = match (arrT comm >>> (id |><| unitT)) undefined
+    => (_1 `m` (v (s (tv a) (tv b)))) `arr` v (tv a)
+baz = caseS (arrT (flipC unit)) (ignore >>> unitT >>> zeroV)
 
 quux :: (Monoidal arr m, O arr tarr m _1 v s p t u, C tarr varr v m t,
          T varr s p tv)
-     => (v (tv a) `m` v (s u u)) `arr` (t (v a))
-quux = (id |><| ignore) >>> arrT (flipC (comm <<< unit <<< tVar))
+     => v (tv a) `arr` t (v a)
+quux = arrT (flipC tVar)
 
 runR :: (Monoidal arr m,
          C tarr varr v m t,
