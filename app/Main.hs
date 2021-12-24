925cf3c33b71a3ebc7eeb861a910c1d0d99cb002
Tom Ellis <tom-git@jaguarpaw.co.uk>
2021-12-24 12:15:17 +0000

    Concurrency more or less works
    but I need to clean up properly

--- app/Main.hs
+++ app/Main.hs
@@ -1,5 +1,6 @@
 {-# OPTIONS_GHC -Wall #-}
 {-# LANGUAGE LambdaCase #-}
+{-# OPTIONS_GHC -Wno-unused-imports #-}
 
 module Main where
 
@@ -12,49 +13,55 @@ import System.Posix.IO.Select
 import System.Posix.IO.Select.Types (Timeout (Never))
 import System.Posix.Types (Fd(Fd))
 import qualified Data.ByteString.Char8 as C
+import Control.Concurrent (forkIO, threadDelay, threadWaitRead)
 
 
 -- See https://stackoverflow.com/questions/29593116/simple-unix-domain-sockets-server
 main :: IO ()
-main = withSocketsDo $ do
-       read  <- socket AF_UNIX Stream 0
+main = do
+       read <- socket AF_UNIX Stream 0
        bind read (SockAddrUnix "/tmp/.X11-unix/X111")
        listen read maxListenQueue
 
-       write <- socket AF_UNIX Stream 0
+       _FIXME_never_get_here <- fix (\acceptAgain -> do
+         print "Waiting for new connection"
+         (conn, _) <- accept read
+         print "Got connection"
+         _ <- forkIO $ do
+           write <- socket AF_UNIX Stream 0
+           connect write (SockAddrUnix "/tmp/.X11-unix/X1000")
 
-       connect write (SockAddrUnix "/tmp/.X11-unix/X1000")
+           print "Opened new socket for writing"
 
-       (conn, _) <- accept read
+           connFd <- Fd <$> unsafeFdSocket conn
+           writeFd <- Fd <$> unsafeFdSocket write
 
-       connFd <- Fd <$> unsafeFdSocket conn
-       writeFd <- Fd <$> unsafeFdSocket write
+           print connFd
+           print writeFd
 
-       _ <- talk conn connFd write writeFd
-       close conn
+           _ <- forkIO $ fix (\readAgain -> do
+             threadWaitRead connFd
+             msg <- recv conn 1024
+             if C.null msg then
+               pure ()
+             else do
+               n <- send write msg
+               print n
+               readAgain)
+
+           _ <- forkIO $ fix (\readAgain -> do
+             threadWaitRead writeFd
+             msg <- recv write 1024
+             if C.null msg then
+               pure ()
+             else do
+               n <- send conn msg
+               print n
+               readAgain)
+
+           -- FIXME: need to close file descriptors
+           pure ()
+
+         acceptAgain)
+
        close read
-       close write
-
-    where
-      talk read readFd write writeFd = go
-        where go = do fix (\again -> do
-                              select' [readFd, writeFd] [] [] Never >>= \case
-                                Nothing -> error "Got error when selecting"
-                                Just (ready, _, _)
-                                  | readFd `elem` ready -> do
-                                      msg <- recv read 1024
-                                      if C.null msg then
-                                        pure ()
-                                        else do
-                                        n <- send write msg
-                                        print n
-                                        again
-                                  | writeFd `elem` ready -> do
-                                      msg <- recv write 1024
-                                      if C.null msg then
-                                        pure ()
-                                        else do
-                                        n' <- send read msg
-                                        print n'
-                                        again
-                                  | otherwise -> again)
