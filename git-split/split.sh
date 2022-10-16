f92e408eeaed7b7035b81d6e32dcbe1121a4e6d7
Tom Ellis <tom-git@jaguarpaw.co.uk>
2022-10-16 09:37:22 +0100

    Fail if there are uncommitted changes

--- git-split/split.sh
+++ git-split/split.sh
@@ -51,6 +51,7 @@ HANDLER=$1
 # This is the best way I know of to (print an error message and then
 # exit) on failure when set -e is set
 git merge-base --is-ancestor $COMBINED $CURRENT || (echo "$COMBINED_PROVIDED is not an ancestor of $CURRENT_BRANCH"; false) || exit
+git diff --quiet || (echo "The repo has uncommitted changes.  Stash, commit or reset them and then try again."; false) || exit
 
 COMBINED_PARENT=$(git rev-parse $COMBINED^)
 COMBINED_PARENT_SHORT=$(git rev-parse --short $COMBINED^)
