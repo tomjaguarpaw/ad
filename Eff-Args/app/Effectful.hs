46ed57c2a3fd727fe76a517fb70fb80ac259e140
Tom Ellis <tom-git@jaguarpaw.co.uk>
2023-01-15 17:11:39 +0000

    List-oriented versions

--- Eff-Args/app/Effectful.hs
+++ Eff-Args/app/Effectful.hs
@@ -56,14 +56,26 @@ thisDivergesFortunately = have undefined `seq` ()
 to :: (eff :> es => t es r) -> s :> es => Has eff s -> t es r
 to k h = case have h of Dict -> k
 
+toL :: (effs ::> es => t es r) -> ss ::> es => Has effs ss -> t es r
+toL k h = case have h of Dict -> k
+
 from ::
   (t (eff : es) a -> t es b) ->
   (forall s. Has eff s -> t (s : es) a) ->
   t es b
 from f k = f (has k)
 
+fromL ::
+  (t (effs +++ es) a -> t es b) ->
+  (forall ss. Has effs ss -> t (ss +++ es) a) ->
+  t es b
+fromL f k = f (has k)
+
 throwError :: s :> es => Has (Error e) s -> e -> Eff es a
 throwError h e = to (Eff.throwError e) h
+
+throwErrorL :: ss ::> es => Has '[Error e] ss -> e -> Eff es a
+throwErrorL h e = toL (Eff.throwError e) h
 
 withReturn ::
   (forall err. Has (Error r) err -> Eff (err : effs) Void) ->
@@ -74,22 +86,48 @@ withReturn f = do
     Right r -> absurd r
     Left l -> l
 
+withReturnL ::
+  (forall err. Has '[Error r] err -> Eff (err +++ effs) Void) ->
+  Eff effs r
+withReturnL f = do
+  r1 <- runErrorNoCallStackL f
+  pure $ case r1 of
+    Right r -> absurd r
+    Left l -> l
+
 runErrorNoCallStack ::
   (forall s. Has (Error e) s -> Eff (s : es) a) ->
   Eff es (Either e a)
 runErrorNoCallStack = from Eff.runErrorNoCallStack
 
+runErrorNoCallStackL ::
+  (forall ss. Has '[Error e] ss -> Eff (ss +++ es) a) ->
+  Eff es (Either e a)
+runErrorNoCallStackL = fromL Eff.runErrorNoCallStack
+
 get :: s :> es => Has (State st) s -> Eff es st
 get = to Eff.get
 
+getL :: ss ::> es => Has '[State st] ss -> Eff es st
+getL = toL Eff.get
+
 put :: s :> es => Has (State st) s -> st -> Eff es ()
 put h st = to (Eff.put st) h
+
+putL :: ss ::> es => Has '[State st] ss -> st -> Eff es ()
+putL h st = toL (Eff.put st) h
 
 evalState ::
   s ->
   (forall e. Has (State s) e -> Eff (e : es) a) ->
   Eff es a
 evalState s = from (Eff.evalState s)
+
+evalStateL ::
+  s ->
+  (forall ee. Has '[State s] ee -> Eff (ee +++ es) a) ->
+  Eff es a
+evalStateL s = fromL (Eff.evalState s)
 
 (!?) :: [a] -> Int -> Maybe a
 xs !? i = runPureEff $
