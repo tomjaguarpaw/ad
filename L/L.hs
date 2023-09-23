1f45b4bfd22da0fc0b12a947f9c47aeda32d420c
Tom Ellis <tom-git@jaguarpaw.co.uk>
2023-09-23 11:38:03 +0100

    Fresh variables

--- L/L.hs
+++ L/L.hs
@@ -16,6 +16,7 @@
 
 module L where
 
+import qualified Control.Monad.Trans.State as State
 import Data.Kind (Constraint, Type)
 
 data Polarity = Positive | Negative
@@ -104,20 +105,27 @@ type family KnownLType t where
   KnownLType (t :: LType Positive) = t ~ Perp (Perp t)
   KnownLType (t :: LType Negative) = t ~ Perp (Perp t)
 
+type M = State.State Int
+
+fresh :: String -> M String
+fresh s = do
+  i <- State.get
+  State.modify' (+ 1)
+  pure (s ++ show i)
+
 -- p5
 lam ::
   forall a b.
   (KnownLType b) =>
   VarId a ->
   Term' b ->
-  Term' (a `Lolly` b)
+  M (Term' (a `Lolly` b))
 -- fixme: fresh variable
-lam x t = MuPair @a @(Perp b) x a comp
-  where
-    a = "alpha"
-
-    comp :: Computation
-    comp = Computation t (Var @(Perp b) a)
+lam x t = do
+  a <- fresh "alpha"
+  let comp :: Computation
+      comp = Computation t (Var @(Perp b) a)
+  pure (MuPair @a @(Perp b) x a comp)
 
 type Term' (t :: LType p) = Term p t
 
@@ -126,25 +134,22 @@ apply ::
   (KnownLType b) =>
   Term' (a `Lolly` b) ->
   Term' a ->
-  Term Negative b
-apply t u =
-  Mu @(Perp b) alpha (Computation t pair)
-  where
-    alpha = "alpha1"
-
-    pair :: Term' (a `Tensor` Perp b)
-    pair = u `Pair` Var alpha
+  M (Term Negative b)
+apply t u = do
+  alpha <- fresh "alpha"
+  let pair :: Term' (a `Tensor` Perp b)
+      pair = u `Pair` Var alpha
+  pure (Mu @(Perp b) alpha (Computation t pair))
 
 -- p20
 thunk ::
   forall n.
   (KnownLType n) =>
   Term Negative n ->
-  Term Positive (Down n)
-thunk t = MuReturn @(Perp n) alpha (Computation t (Var @(Perp n) alpha))
-  where
-    alpha :: VarId (Perp n)
-    alpha = "alpha2"
+  M (Term Positive (Down n))
+thunk t = do
+  alpha <- fresh "alpha"
+  pure (MuReturn @(Perp n) alpha (Computation t (Var @(Perp n) alpha)))
 
 -- p22
 to ::
@@ -153,26 +158,28 @@ to ::
   (KnownLType n) =>
   Term' (Up a) ->
   VarId a ->
-  Term Negative n ->
-  Term' n
-(t `to` x) u = Mu @(Perp n) alpha comp2
-  where
-    alpha = "alpha3"
-    comp1 = Computation u (Var @(Perp n) alpha)
-    comp2 = Computation t (MuReturn @a x comp1)
+  M
+    ( Term Negative n ->
+      Term' n
+    )
+t `to` x = do
+  alpha <- fresh "alpha"
+  pure $ \u ->
+    let comp1 = Computation u (Var @(Perp n) alpha)
+        comp2 = Computation t (MuReturn @a x comp1)
+     in Mu @(Perp n) alpha comp2
 
 -- p22
 force ::
   forall n.
   (KnownLType n) =>
   Term Positive (Down n) ->
-  Term Negative n
-force t = Mu @(Perp n) alpha (Computation returnAlpha t)
-  where
-    alpha = "alpha"
-
-    returnAlpha :: Term Negative (Up (Perp n))
-    returnAlpha = Return (Var alpha)
+  M (Term Negative n)
+force t = do
+  alpha <- fresh "alpha"
+  let returnAlpha :: Term Negative (Up (Perp n))
+      returnAlpha = Return (Var alpha)
+  pure (Mu @(Perp n) alpha (Computation returnAlpha t))
 
 -- p23
 cbvLam ::
@@ -181,8 +188,9 @@ cbvLam ::
   (KnownLType (Perp a `Dna` Up b)) =>
   VarId a ->
   Term Negative (Up b) ->
-  Term Negative (Up (Down (a `Lolly` Up b)))
-cbvLam x t = Return (thunk (lam @a x t))
+  M (Term Negative (Up (Down (a `Lolly` Up b))))
+cbvLam x t =
+  Return <$> (thunk =<< lam @a x t)
 
 -- p21
 cbaApply ::
@@ -192,9 +200,8 @@ cbaApply ::
   (KnownLType (Perp a `Dna` Up b)) =>
   Term Negative (Up (Down (a `Lolly` Up b))) ->
   Term Negative (Up a) ->
-  Term Negative (Up b)
-cbaApply t u =
-  u `to` x $ t `to` f $ force (Var f) `apply` Var @a x
-  where
-    x = "x"
-    f = "f"
+  M (Term Negative (Up b))
+cbaApply t u = do
+  x <- fresh "x"
+  f <- fresh "f"
+  (u `to` x) <*> ((t `to` f) <*> (do f' <- force (Var f); apply f' (Var @a x)))
