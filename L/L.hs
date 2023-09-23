50879d3e8840b16d79384e4c7da8fe92de5cf227
Tom Ellis <tom-git@jaguarpaw.co.uk>
2023-09-23 13:24:27 +0100

    WIP towards evaluation

--- L/L.hs
+++ L/L.hs
@@ -2,9 +2,11 @@
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE GADTs #-}
 {-# LANGUAGE KindSignatures #-}
+{-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE PolyKinds #-}
 {-# LANGUAGE RankNTypes #-}
 {-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE StandaloneDeriving #-}
 {-# LANGUAGE StandaloneKindSignatures #-}
 {-# LANGUAGE TypeApplications #-}
 {-# LANGUAGE TypeFamilies #-}
@@ -16,8 +18,10 @@
 
 module L where
 
+import Control.Monad.Trans.Class (lift)
 import qualified Control.Monad.Trans.State as State
 import Data.Kind (Constraint, Type)
+import qualified Data.Map.Strict as Map
 
 data Polarity = Positive | Negative
 
@@ -49,9 +53,33 @@ data LType p where
   WhyNot :: LType Negative -> LType Negative
   Up :: LType Positive -> LType Negative
 
+type SLType' (t :: LType p) = SLType p t
+
+type SLType :: forall (p :: Polarity) -> LType p -> Type
+data SLType p a where
+  STensor ::
+    SLType' a ->
+    SLType' b ->
+    SLType' (a `Tensor` b)
+  SDown :: SLType' a -> SLType' (Down a)
+  SBottom :: SLType' Bottom
+
+class KnownLType' (a :: LType p) where
+  know :: SLType p a
+
+instance (KnownLType' a, KnownLType' b) => KnownLType' (a `Tensor` b) where
+  know = STensor know know
+
+instance KnownLType' Bottom where
+  know = SBottom
+
+instance (KnownLType' a) => KnownLType' (Down a) where
+  know = SDown know
+
 type Perp :: forall (p :: Polarity). LType p -> LType (Flip p)
 type family Perp t = t' {- can't do | t' -> t -} where
   Perp (TyVar a) = TyVarPerp a
+  Perp (TyVar a) = TyVarPerp' a
   Perp (TyVarPerp a) = TyVar a
   Perp (TyVarPerp' a) = TyVar a
   Perp (a `Tensor` b) = Perp a `Dna` Perp b
@@ -93,9 +121,35 @@ data Term p t where
   EmptyCase :: Term Negative Top
   Return :: Term Positive a -> Term Negative (Up a)
   MuReturn :: VarId a -> Computation -> Term Positive (Down (Perp a))
+  -- Not sure how to stop the computation
+  Stop :: (KnownLType' a) => Term p a
+
+deriving instance Show (Term p t)
 
 data Computation where
   Computation :: Term Negative (Perp a) -> Term Positive a -> Computation
+
+deriving instance Show Computation
+
+showTerm :: Term p t -> String
+showTerm = \case
+  Var v -> v
+  Mu v c -> "μ" ++ v ++ ". " ++ showComputation c
+  Pair t1 t2 -> "(" ++ showTerm t1 ++ ", " ++ showTerm t2 ++ ")"
+  MuPair v1 v2 c -> "μ(" ++ v1 ++ ", " ++ v2 ++ "). " ++ showComputation c
+  Unit -> "()"
+  MuUnit {} -> error "MuUnit"
+  OneDot {} -> error "OneDot"
+  TwoDot {} -> error "TwoDot"
+  MuDot {} -> error "MuDot"
+  EmptyCase {} -> error "EmptyCase"
+  Return t -> "⇑" ++ showTerm t
+  MuReturn v c -> "μ⇑" ++ v ++ "." ++ showComputation c
+  Stop -> "Stop"
+
+showComputation :: Computation -> String
+showComputation (Computation t1 t2) =
+  "〈" ++ showTerm t1 ++ "|" ++ showTerm t2 ++ "〉"
 
 type Lolly :: LType Positive -> LType Negative -> LType Negative
 type Lolly a b = Perp a `Dna` b
@@ -107,6 +161,9 @@ type family KnownLType t where
   KnownLType (t :: LType Negative) = t ~ Perp (Perp t)
 
 type M = State.State Int
+
+runM :: M a -> a
+runM = flip State.evalState 0
 
 fresh :: String -> M (VarId a)
 fresh s = do
@@ -123,7 +180,7 @@ lam ::
   M (Term' (a `Lolly` b))
 -- fixme: fresh variable
 lam x t = do
-  a <- fresh "alpha"
+  a <- fresh "α"
   let comp :: Computation
       comp = Computation t (Var @(Perp b) a)
   pure (MuPair @a @(Perp b) x a comp)
@@ -137,7 +194,7 @@ apply ::
   Term' a ->
   M (Term Negative b)
 apply t u = do
-  alpha <- fresh "alpha"
+  alpha <- fresh "α"
   let pair :: Term' (a `Tensor` Perp b)
       pair = u `Pair` Var alpha
   pure (Mu @(Perp b) alpha (Computation t pair))
@@ -149,7 +206,7 @@ thunk ::
   Term Negative n ->
   M (Term Positive (Down n))
 thunk t = do
-  alpha <- fresh "alpha"
+  alpha <- fresh "α"
   pure (MuReturn @(Perp n) alpha (Computation t (Var @(Perp n) alpha)))
 
 -- p22
@@ -164,7 +221,7 @@ to ::
       Term' n
     )
 t `to` x = do
-  alpha <- fresh "alpha"
+  alpha <- fresh "α"
   pure $ \u ->
     let comp1 = Computation u (Var @(Perp n) alpha)
         comp2 = Computation t (MuReturn @a x comp1)
@@ -177,7 +234,7 @@ force ::
   Term Positive (Down n) ->
   M (Term Negative n)
 force t = do
-  alpha <- fresh "alpha"
+  alpha <- fresh "α"
   let returnAlpha :: Term Negative (Up (Perp n))
       returnAlpha = Return (Var alpha)
   pure (Mu @(Perp n) alpha (Computation returnAlpha t))
@@ -216,16 +273,77 @@ type CBVLolly a b = Down (a `Lolly` Up b)
 
 exampleTerm ::
   forall a0 a1 b.
-  (KnownLType a0) =>
-  (KnownLType a1) =>
+  (KnownLType (a0 :: LType Positive)) =>
+  (KnownLType (a1 :: LType Positive)) =>
   (KnownLType b) =>
-  M (Term' (CBVType (a0 `CBVLolly` (a1 `CBVLolly` b))))
+  M (Term' (CBVType b))
 exampleTerm = do
-  x <- fresh "one"
-  y <- fresh "two"
-  minus <- fresh "-"
+  x <- fresh "x"
+  y <- fresh "y"
+  one <- fresh "one"
+  two <- fresh "two"
+  minus <- fresh "sub"
 
   inner <-
     cbvApply <$> cbvApply (cbvVar minus) (cbvVar @a0 x) <*> pure (cbvVar @a1 y)
 
-  cbvLam @a0 x =<< cbvLam @a1 y =<< inner
+  lam_ <- cbvLam @a0 x =<< (cbvLam @a1 y =<< inner)
+
+  applied <- cbvApply <$> cbvApply lam_ (cbvVar @a0 one) <*> pure (cbvVar @a1 two)
+  applied
+
+data TypedTerm p where
+  TypedTerm ::
+    forall (p :: Polarity) (t :: LType p).
+    SLType p t ->
+    Term p t ->
+    TypedTerm p
+
+-- This is silly and expensive.  We should store the type with each
+-- constructor.
+typedTerm :: Term p t -> TypedTerm p
+typedTerm = \case
+  Var {} -> error "Var"
+  Mu {} -> error "Mu"
+  Pair {} -> error "Pair"
+  MuPair {} -> error "MuPair"
+  Unit {} -> error "Unit"
+  MuUnit {} -> error "MuUnit"
+  OneDot {} -> error "OneDot"
+  TwoDot {} -> error "TwoDot"
+  MuDot {} -> error "MuDot"
+  EmptyCase {} -> error "EmptCase"
+  Return {} -> error "Retur"
+  MuReturn {} -> error "MuReturn"
+
+type Subst = Map.Map String (TypedTerm Positive)
+
+-- p21
+-- These are the opposite way round!
+step :: (Monad m) => Computation -> State.StateT Subst m (Maybe Computation)
+step (Computation (Mu x c) t) = do
+  State.modify' (Map.insert x (typedTerm t))
+  pure (Just c)
+step c = error (show c)
+
+type TermType = CBVType One
+
+example :: IO ()
+example = do
+  let term :: Term' TermType
+      term = runM (exampleTerm @One @One @One)
+
+  putStrLn (showTerm term)
+
+  let loop c =
+        step c >>= \case
+          Nothing -> pure ()
+          Just c' -> do
+            lift (putStrLn (showComputation c'))
+            loop c'
+
+  flip
+    State.evalStateT
+    Map.empty
+    -- This type argument is annoying
+    (loop (Computation @(Perp TermType) term Stop))
