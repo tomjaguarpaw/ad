25e014ad7aa4b3be4639fd9b9131ea3e07c41b93
Tom Ellis <tom-git@jaguarpaw.co.uk>
2023-11-17 14:07:43 +0000

    Cabal package

---
+++ Term/app/Main.hs
@@ -0,0 +1,52 @@
+import Data.ByteString
+import System.IO
+import Data.Function (fix)
+import System.Process
+import System.Posix.IO
+import System.Posix.Terminal
+import Control.Concurrent
+
+openPty = do
+  (m, s) <- openPseudoTerminal
+  (,) <$> fdToHandle m <*> fdToHandle s
+
+main = do
+  Prelude.putStrLn "Starting ..."
+  hSetEcho stdin False
+  hSetBuffering stdin NoBuffering
+
+{-
+  (si, mi) <- openPty
+  (so, mo) <- openPty
+  (se, me) <- openPty
+
+  _ <-
+    createProcess (proc "cat" []) { std_in = UseHandle si,
+                                     std_out = UseHandle so,
+                                     std_err = UseHandle se
+                                   }
+-}
+
+  (Just mi, Just mo, Just me, _) <-
+    createProcess (proc "cat" []) { std_in = CreatePipe,
+                                     std_out = CreatePipe,
+                                     std_err = CreatePipe
+                                   }
+
+  forkIO $ fix $ \again -> do
+    bs <- hGet me 1
+    hPut stderr bs
+    hFlush stderr
+    again
+
+  forkIO $ fix $ \again -> do
+    bs <- hGet mo 1
+    hPut stdout bs
+    hFlush stdout
+    again
+
+  fix $ \again -> do
+    bs <- hGet stdin 1
+    hPut mi bs
+    hFlush mi
+    again
