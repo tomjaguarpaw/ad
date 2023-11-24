7decec00ae748972b642893265092b25a6dd5594
Tom Ellis <tom-git@jaguarpaw.co.uk>
2023-11-24 23:12:15 +0000

    Draw a little status bar

--- Term/app/Main.hs
+++ Term/app/Main.hs
@@ -25,11 +25,11 @@ main = do
   hSetEcho stdin False
   hSetBuffering stdin NoBuffering
 
-  sz <- do
+  (cols, rows) <- do
     Just stdInPty <- Pty.createPty 0
     Pty.ptyDimensions stdInPty
 
-  (pty, _) <- Pty.spawnWithPty Nothing False "/bin/bash" [] sz
+  (pty, _) <- Pty.spawnWithPty Nothing False "/bin/bash" [] (cols, subtract 1 rows)
 
   _ <- flip (installHandler keyboardSignal) Nothing . Catch $ do
     Pty.writePty pty (pack [3])
@@ -69,19 +69,22 @@ main = do
       readEither >>= \case
         StdIn bs -> do
           Pty.writePty pty bs
+        PtyIn bs -> do
+          hPut stdout bs
           hPut stdout (C8.pack "\o33[6n")
-          _ <- flip fix mempty $ \again' sofar -> do
+          hFlush stdout
+          (x, y) <- flip fix mempty $ \again' sofar -> do
             b <- hGet stdin 1
             if b == C8.pack "R"
-              then
-                let (x, Data.ByteString.drop 1 -> y) =
+              then -- Drop [ and drop ;
+                let (Data.ByteString.drop 1 -> x, Data.ByteString.drop 1 -> y) =
                       C8.break (== ';') (Data.ByteString.drop 1 sofar)
                  in pure (read (C8.unpack x) :: Int, read (C8.unpack y) :: Int)
               else again' (sofar <> b)
-          pure ()
-        PtyIn bs -> do
-          hPut stdout bs
-          hFlush stdout
+          hPut stdout (C8.pack ("\o33[" <> show rows <> ";1H"))
+          hPut stdout (C8.pack "\o33[K")
+          hPut stdout (C8.pack ("A status bar: " <> show x <> " and " <> show y))
+          hPut stdout (C8.pack ("\o33[" <> show x <> ";" <> show y <> "H"))
 
       again
 
