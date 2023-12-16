b9fe13a5a173c6dca7915745d0e786d23f9a9a63
Tom Ellis <tom-git@jaguarpaw.co.uk>
2023-12-16 17:01:02 +0000

    Test wrapnext

---
+++ Term/test_wrapnext.zsh
@@ -0,0 +1,15 @@
+# Wrapnext shouldn't be changed if we emit an escape code that doesn't
+# move the cursor
+
+for i in $(seq 1 $LINES); do
+    echo
+done
+
+echo "Should not see X overlapping the bar"
+
+/usr/bin/echo -n "<"
+for i in `seq 2 $[COLUMNS-1]`; do
+    /usr/bin/echo -n " "
+done
+/usr/bin/echo -n -e ">X"
+sleep 2
