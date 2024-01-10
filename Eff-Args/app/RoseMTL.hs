b5b0204262f60a3f9915cfd850a93f65a307203b
Tom Ellis <tom-git@jaguarpaw.co.uk>
2024-01-10 17:42:11 +0000

    Inclusion operators

--- Eff-Args/app/RoseMTL.hs
+++ Eff-Args/app/RoseMTL.hs
@@ -22,7 +22,7 @@
 
 module RoseMTL where
 
-import Control.Monad.Morph (hoist)
+import Control.Monad.Morph (hoist, MFunctor)
 import Control.Monad.Trans (MonadTrans (lift))
 import Control.Monad.Trans.State.Strict as State
 import Data.Kind (Type)
@@ -31,13 +31,13 @@ data Rose a = Branch (Rose a) (Rose a) | Leaf a | Empty
 
 data SRose i where
   SBranch :: (SingI i1, SingI i2) => SRose (Branch i1 i2)
-  SLeaf :: (MonadTrans t) => SRose (Leaf t)
+  SLeaf :: (MonadTrans t, MFunctor t) => SRose (Leaf t)
   SEmpty :: SRose Empty
 
 class SingI i where
   sing :: SRose i
 
-instance (MonadTrans t) => SingI (Leaf t) where
+instance (MonadTrans t, MFunctor t) => SingI (Leaf t) where
   sing = SLeaf
 
 instance (SingI t1, SingI t2) => SingI (Branch t1 t2) where
@@ -49,6 +49,22 @@ instance SingI Empty where
 type (:&) = 'Branch
 
 type Effect = (Type -> Type) -> Type -> Type
+
+class (a :: Rose Effect) :> (b :: Rose Effect) where
+  embed :: (Monad m) => Eff a m r -> Eff b m r
+
+instance {-# INCOHERENT #-} e :> e where
+  embed = id
+
+instance (SingI x, SingI es, (e :> es)) => e :> (x :& es) where
+  embed = EffBranch . lift . embed
+
+-- Do we want this?
+-- instance {-# incoherent #-} (e :> es) => (e' :& e) :> (e' :> es)
+
+-- This seems a bit wobbly
+instance {-# INCOHERENT #-} (SingI e, SingI es) => e :> (e :& es) where
+  embed = EffBranch . hoist lift
 
 data Eff (es :: Rose Effect) m a where
   EffEmpty :: m a -> Eff Empty m a
@@ -83,6 +99,12 @@ instance (SingI es) => MonadTrans (Eff es) where
     SEmpty -> EffEmpty
     SLeaf -> EffLeaf . lift
     SBranch -> EffBranch . lift . lift
+
+instance SingI es => MFunctor (Eff es) where
+  hoist f = case sing @es of
+    SEmpty -> \(EffEmpty m) -> EffEmpty (f m)
+    SLeaf -> \(EffLeaf m) -> EffLeaf (hoist f m)
+    SBranch -> \(EffBranch m) -> EffBranch (hoist (hoist f) m)
 
 handleState ::
   (SingI effs, Monad m) =>
