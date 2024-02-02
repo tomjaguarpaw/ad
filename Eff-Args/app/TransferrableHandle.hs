62e663240127f79ea8cf3917c76fe4fbd0870ec6
Tom Ellis <tom-git@jaguarpaw.co.uk>
2024-02-02 11:18:54 +0000

    Implement uncons -- it's very complicated

--- Eff-Args/app/TransferrableHandle.hs
+++ Eff-Args/app/TransferrableHandle.hs
@@ -6,11 +6,15 @@ module TransferrableHandle where
 
 import Control.Concurrent
   ( MVar,
+    killThread,
     newEmptyMVar,
     putMVar,
     takeMVar,
   )
+import Control.Monad (join)
+import Data.Foldable (for_)
 import Data.Function (fix)
+import Data.Traversable (for)
 import GHC.Conc (forkIO)
 import Prelude hiding (break)
 
@@ -55,15 +59,95 @@ anH h = do
 
   pure "Hello"
 
-foo :: IO ()
-foo = do
+example1 :: IO ()
+example1 = do
   (b, rest) <- runH (\h -> break h anH)
   print ("Broke on", b)
   r <- runH rest
   print r
 
+example2 :: IO ()
+example2 = allViaUncons anH
+
 forever :: (a -> IO b) -> a -> IO (b, H a b)
 forever f = lify f (forever f)
+
+allViaUncons :: (Show a, Show r) => (H a () -> IO r) -> IO ()
+allViaUncons s = do
+  uncons s >>= \case
+    Left r -> print r
+    Right (a, rest) -> do
+      print a
+      allViaUncons rest
+
+race :: [IO r] -> IO r
+race jobs = do
+  result <- newEmptyMVar
+  tids <- for jobs $ \job ->
+    forkIO (job >>= putMVar result)
+  r <- takeMVar result
+  for_ tids killThread
+  pure r
+
+uncons ::
+  (H a () -> IO r) ->
+  IO (Either r (a, H a () -> IO r))
+uncons f = do
+  rvar <- newEmptyMVar
+  avar <- newEmptyMVar
+
+  ref <- do
+    kify
+      ( -- When the handler receives an a, the worker thread is
+        -- implicitly paused
+        \put -> put $ \a -> do
+          -- We allocate a new handler
+          kify
+            ( \put' -> do
+                -- We run the setup of the new handler in the main
+                -- thread, because it determines the return value of
+                -- uncons.  Because the worker thread is paused it
+                -- can't return an "r" during this time, i.e. rvar
+                -- will not become filled.
+                putMVar avar $ do
+                  pure
+                    ( Right
+                        ( -- We return the head
+                          a,
+                          -- and the "rest" handler is passed in by the
+                          -- caller of uncons
+                          \(MkH v') -> do
+                            -- Unwrap the "rest" handler
+                            v'' <- takeMVar v'
+                            -- We place the "rest" handler inside the
+                            -- new handler.  This implicitly unpauses
+                            -- the thread, because it now has
+                            -- something it can successfully takeMVar
+                            -- of.
+                            put' v''
+                            -- We wait for the thread to complete
+                            takeMVar rvar
+                        )
+                    )
+            )
+            -- Return () and the new handler
+            (\h -> pure ((), h))
+      )
+      pure
+
+  -- Run the worker thread
+  _ <- forkIO $ do
+    r <- f ref
+    putMVar rvar r
+
+  join $
+    race
+      [ takeMVar avar,
+        do
+          r <- takeMVar rvar
+          pure $ do
+            pure (Left r)
+      ]
 
 break ::
   forall a b r.
